e606ca8be1e1ee9dbab1d2d65e15edbe6e864ee0
hannahsberry <85853704+hannahsberry@users.noreply.github.com>
2021-08-02 22:22:40 +1000

    Hamburger Menu

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+
+    $(".hamburger-button").click(function(){
+
+        $(this).toggleClass('active');
+        $(".mobile-menu").fadeToggle();
+
+});
